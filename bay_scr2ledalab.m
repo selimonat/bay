68052cea21051c5ea04a1975012a7bb9a524091e
selimonat <onatselim@gmail.com>
2015-03-13 14:35:01 +0100

    added functionality for binned single-trial analysis

--- bay_scr2ledalab.m
+++ bay_scr2ledalab.m
@@ -1,88 +1,154 @@
+function out = bay_scr2ledalab(whattodo,varargin)
 
-%%
-clear all
-for session = 1:2;
-    for subject = 2:19
-        path         =  '/Users/onat/Desktop/toLedalab/';
-        fname        = sprintf('%stoLedalab_BayBP_S%d_%02d.mat',path,session,subject);
-        fname_save   = sprintf('%s%s/toLedalab_BayBP_S%d_%02d.mat',path,'corrected',session,subject);
-        if exist(fname_save) == 0;
-            mkdir(fileparts(fname_save));
-        end
-        load(fname);
-        for e = 1:length(data.event)
-            nid  = data.event(e).nid;
-            name = data.event(e).name;
-            if nid == 24%cue+
-                data.event(e).nid = 3;%tens/cue/stim
-            elseif nid == 44%heat+
-                data.event(e).nid = 4;
-            elseif nid == 23%cue-
-                data.event(e).nid = 1;
-            elseif nid == 43%heat-
-                data.event(e).nid = 2;
-            end
-            if strcmp(name,'W')
-                data.event(e).name = '+/heat';
-            elseif strcmp(name,'T')
-                data.event(e).name = '+/cue'
-            elseif strcmp(name,'H')
-                data.event(e).name = '-/heat'
-            elseif strcmp(name,'N')
-                data.event(e).name = '-/cue';
-            end
-        end
-        save(fname_save,'data');
-    end
-end
-%%
+%data path
 path         =  '/Users/onat/Dropbox/Bay_SCR_Ladalab/';
-for session      = 1;
-    for subject      = 3
+out = [];
+%% read and make Ledalab analysis
+if strcmp(whattodo,'run');
+    
+    sessions = varargin{1};
+    subjects = varargin{2};
+    
+    for session   = sessions;
+        for subject  = subjects
+            fprintf('Running: Session: %03d; Subject: %03d...\n',session,subject);
             try
                 fname        = sprintf('%stoLedalab_BayBP_S%d_%02d.mat',path,session,subject);
                 Ledalab({fname}, 'open', 'mat','analyze','CDA', 'optimize',6, 'overview',  1, 'export_era', [-1 15 0 1], 'export_scrlist', [0 1], 'export_eta', 1);
             end
+        end
     end
-end
-%%
-clear all;
-path         =  '/Users/onat/Desktop/toLedalab/corrected/';
-session = 1;
-for subject = 2:19;
-    try
-        fname   = sprintf('%stoLedalab_BayBP_S%d_%02d_results.mat',path,session,subject);
-        load(fname);
-        d = analysis.split_driver
-        figure;
-        plot(d.x(:,1:4),d.mean)
+    
+elseif strcmp(whattodo,'load_results');
+    %
+    session      = varargin{1};
+    subject      = varargin{2};
+    fname        = sprintf('%stoLedalab_BayBP_S%d_%02d_results.mat',path,session,subject);
+    if exist(fname) ~= 0
+        out          = load(fname);
+    else
+        fprintf('File %s doesn''t exist.\n',fname);
+        return
     end
+    
+elseif strcmp(whattodo,'plot');
+    close all
+    ffigure
+    subplot('position',[0.025 0.15 0.95 0.8])
+    session      = varargin{1};
+    subject      = varargin{2};
+    d            = bay_scr2ledalab('load_results',session,subject);
+    fname        = sprintf('%stoLedalab_BayBP_S%d_%02d_plot.png',path,session,subject);
+    if ~isempty(d)
+        %plot the tonic and phasic drive
+        plot(d.data.time.data, d.data.conductance.data,'k.-');%the raw data
+        hold on
+        plot(d.data.time.data, d.analysis.phasicData,'r','linewidth',2);
+        plot(d.data.time.data, d.analysis.tonicData ,'m','linewidth',2);
+        plot(d.data.time.data(1:length( d.analysis.kernel)),d.analysis.kernel);
+        m = d.analysis.driver;
+        %plot(data.time,zscore(analysis.driver)*std(analysis.phasicData)+m,'b','linewidth',1);
+        h = area(d.data.time.data,zscore(d.analysis.driver)*std(d.analysis.phasicData)+m,0,'facecolor','b','linewidth',1,'edgecolor','b');
+        alpha(0.3)
+        box off
+        %
+        xl = get(gca,'xlim');
+        yl = get(gca,'ylim');
+        axis tight
+        title(sprintf('Subject: %d, Phase: %d, CompoundError: %0.3g,Negativity:%0.3g',subject,session,d.analysis.error.negativity,d.analysis.error.compound))
+        
+        %% plot a line at stimulus onset.
+        tface = 4;
+        colors = [0 0 0; 1 0 0; .5 .5 .5; 0 0 1];
+        for ne = 1:length(d.data.event);
+            %find the color that corresponds to the current contiion
+            cond = d.data.event(ne).nid;
+            if isnan(cond) == 0
+                color = colors( cond,:);
+            else
+                color = [ 0 0 0];
+                cond  = 10;
+            end
+            %plot stimulus onsets
+            patchline(repmat(d.data.event(ne).time - d.data.time.timeoff,1,2),ylim,'edgecolor',color,'linewidth',2,'edgealpha',0.25);
+        end
+        xlabel('time (seconds)')
+        
+        %% plot the kernel as an inset
+        hold on
+        axes('position',[0.75 0.75 0.2 0.2])
+        
+        %plot the bateman
+        plot(d.data.time.data(1:length( d.analysis.kernel)),d.analysis.kernel,'k','linewidth',2);
+        title(sprintf('Bateman(%0.3g,%0.3g)',d.analysis.tau(1),d.analysis.tau(2)))
+        xlabel('time (s)')
+        box off
+        
+        %% save the stuff
+        SaveFigure(fname)
+    end
+    
+elseif strcmp(whattodo,'get_timecourse')    
+    target_timebin = varargin{3};
+    session        = varargin{1};
+    counter        = 0;
+    Mbinned        = [];
+    for subject        = varargin{2};
+        fprintf('Processing subject #%03d...\n',subject);
+        counter = counter + 1;
+        %extract the data triggered with heat onset
+        out     = bay_scr2ledalab('load_results',session,subject);
+        if ~isempty(out)
+            M       = out.analysis.split_driver.y;
+            c       = out.analysis.split_driver.c;
+            M       = M(:,ismember(c,[2 4]));
+            %create a binning matrix
+            [BM,centers]=BinningMatrix(size(M,1),target_timebin);
+            Mbinned(:,:,counter)       = BM'*M;
+        end
+    end
+    
+    out = Mbinned;
 end
-%%
 
-%%
-%color code for tens vs. no-tens
-co{[43]} = 'r';
-co{[44]} = 'b';
-co{[23]} = 'r';
-co{[24]} = 'b';
-%
-t{[1]}   = { 'cue'};
-t{[21]}  = { 'heat'};
-c        = 0;
-%%
-figure(100);
-c    = 0;
-for cue = [ 0 20];
-    c = c + 1;
-    subplot(1,2,c);
-    for block = [23 24]+cue
-        i = analysis.split_phasicData.c == block ;
-        plot(mean(analysis.split_phasicData.y(:,i),2),'color',co{block})
-        hold on;
-    end
-    [cue block]
-    title(t{cue+1});
-    legend('tens-','tens+');
-    hold off
-end
+% % %%
+% % clear all;
+% % path         =  '/Users/onat/Desktop/toLedalab/corrected/';
+% % session = 1;
+% % for subject = 2:19;
+% %     try
+% %         fname   = sprintf('%stoLedalab_BayBP_S%d_%02d_results.mat',path,session,subject);
+% %         load(fname);
+% %         d = analysis.split_driver
+% %         figure;
+% %         plot(d.x(:,1:4),d.mean)
+% %     end
+% % end
+% % %%
+% %
+% % %%
+% % %color code for tens vs. no-tens
+% % co{[43]} = 'r';
+% % co{[44]} = 'b';
+% % co{[23]} = 'r';
+% % co{[24]} = 'b';
+% % %
+% % t{[1]}   = { 'cue'};
+% % t{[21]}  = { 'heat'};
+% % c        = 0;
+% % %%
+% % figure(100);
+% % c    = 0;
+% % for cue = [ 0 20];
+% %     c = c + 1;
+% %     subplot(1,2,c);
+% %     for block = [23 24]+cue
+% %         i = analysis.split_phasicData.c == block ;
+% %         plot(mean(analysis.split_phasicData.y(:,i),2),'color',co{block})
+% %         hold on;
+% %     end
+% %     [cue block]
+% %     title(t{cue+1});
+% %     legend('tens-','tens+');
+% %     hold off
+% % end
